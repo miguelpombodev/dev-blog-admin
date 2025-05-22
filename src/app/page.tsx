f297e468e2a931c67951966403526db2232382c1
Miguel Pombo <miguel.pomboshape@modec.com>
2025-05-22 10:43:30 -0300

    fix: fix env var value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="flex items-center justify-center">
       <a
-        href={`${process.env.APP_URL}/auth/github`}
+        href={`${process.env.NEXT_PUBLIC_APP_URL}/auth/github`}
         className="flex gap-2 px-4 py-2 bg-black text-white rounded-[10] border-1 border-white font-bold"
       >
         <Github width={20} height={20} />
